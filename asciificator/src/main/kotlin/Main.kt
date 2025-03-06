772610097ef543389e01fef4265210bec9ba033a
Lanik42 <Lanik142@gmail.com>
2025-03-06 23:21:14 +0700

    Фикс рандомной строки

--- asciificator/src/main/kotlin/Main.kt
+++ asciificator/src/main/kotlin/Main.kt
@@ -57,7 +57,7 @@ private val VIDEO_EXTENSIONS = listOf("mp4", "avi", "webm", "mkv")
 
 private fun runProcessing(inputArgs: InputArgs) {
     val video = inputArgs.path.substringAfterLast(".") in VIDEO_EXTENSIONS
-    val camera = inputArgs.path == "src/cameramera"
+    val camera = inputArgs.path == "camera"
 
     when {
         camera -> CameraProcessor(inputArgs).start(inputArgs)
